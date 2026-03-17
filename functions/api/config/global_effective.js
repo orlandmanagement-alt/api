6e208d335016b7abb096b89fb4b71eba43030883
orlandmanagement-alt <orlandmanagement@gmail.com>
2026-03-17 11:27:28 +0700

    Delete functions directory

--- functions/api/config/global_effective.js
+++
@@ -1,9 +0,0 @@
-import {json} from "../../_lib.js"
-import {getEffectiveGlobalSettings} from "./_helper/config_service.js"
-
-export async function onRequestGet({env}){
-
-  const data=await getEffectiveGlobalSettings(env)
-
-  return json(200,"ok",data)
-}
